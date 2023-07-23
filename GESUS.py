e237c43217260a8cb81d9b0d6dcac54cafef709e
gabryplayer <138307131+gabryplayer@users.noreply.github.com>
2023-07-23 13:05:28 +0200

    Add files via upload

---
+++ GESUS.py
@@ -0,0 +1,44 @@
+import os
+import time
+print("                                                                     ")
+print("                                                                     ")
+print("                                                                     ")
+print("                                                                     ")
+print("                                                                     ")
+print("                                                                     ")
+print("        GGGGGGGGGG    EEEEEEEEE   SSSSSSSS  UUU   UUU   SSSSSSSSSS")
+print("        GGG           EE          SSS       UUU   UUU   SSS       ")
+print("        GGG           EE          SSS       UUU   UUU   SSS       ")
+print("        GGG           EE          SSS       UUU   UUU   SSS       ")
+print("        GGG   GGGG    EEEEEEEEE   SSSSSSSS  UUU   UUU   SSSSSSSSSS ")
+print("        GGG      G    EE               SSS  UUU   UUU          SSS ")
+print("        GGG      G    EE               SSS  UUU   UUU          SSS ")
+print("        GGG      G    EE               SSS  UUU   UUU          SSS ")
+print("        GGGGGGGGGG    EEEEEEEEE   SSSSSSSS  UUUUUUUUU   SSSSSSSSSS  .py")
+print("......................................................................")
+print("                                         by ILGAB on YouTube")
+print("......................................................................")
+print("                                                                     ")
+print("                                                                     ")
+
+message = input("set a link url: ")
+
+link = input("do you chose the true link?y/n")
+
+if link == "y":
+    print("1")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("3")
+    print("ping", message)
+else:
+    print("OK you can restart the file: ")
+   
+
+    
+    
+
+
+
+
